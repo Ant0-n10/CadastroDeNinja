03d3e0deee03d52769c820dd3d19de91107a0c1b
Ant0-n10 <antonio170pina@gmail.com>
2025-05-12 16:29:50 -0300

    First Commit

---
+++ src/test/java/Ant0_n10/Java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package Ant0_n10.Java10x.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
