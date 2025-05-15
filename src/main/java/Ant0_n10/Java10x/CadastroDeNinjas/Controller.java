28bc231be25a06d75e10becde0673335087a7aa2
Ant0-n10 <antonio170pina@gmail.com>
2025-05-15 19:42:58 -0300

    adding the first controller

---
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package Ant0_n10.Java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Bem Vindo a Primeira rota da aplicação";
+    }
+}
