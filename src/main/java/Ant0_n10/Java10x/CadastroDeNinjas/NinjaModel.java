f46a88c9f2ffd5d18c34083da39420f5a76ebdff
Ant0-n10 <antonio170pina@gmail.com>
2025-05-16 18:08:31 -0300

    JPA dependency added and NinjaModel entity added

---
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,47 @@
+package Ant0_n10.Java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+
+    public NinjaModel() {}
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
