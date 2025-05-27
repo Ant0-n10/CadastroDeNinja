aff9b031eb22bc541026157cee5471ac57af4982
Ant0-n10 <antonio170pina@gmail.com>
2025-05-27 14:29:13 -0300

    feat: Cria o Missoes Repository

---
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,7 @@
+package Ant0_n10.Java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
+
