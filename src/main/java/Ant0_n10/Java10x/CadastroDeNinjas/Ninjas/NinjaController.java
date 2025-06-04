d2d1a51ec358a78daf4c04b6c2def0dbc31e50ca
Ant0-n10 <antonio170pina@gmail.com>
2025-06-03 22:16:19 -0300

    feat: Update controller with mock data

--- src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package Ant0_n10.Java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,31 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Bem Vindo a Primeira rota da aplicação";
+    }
+
+    //Create
+    @PostMapping("/criar")
+    public String criaNinja(){
+        return "Ninja Criado";
+    }
+    //Read
+    @GetMapping("/mostraNinjas")
+    public String mostraNinjas(){
+        return "Todos";
+    }
+    //Read by ID
+    @GetMapping("/mostraNinjasId")
+    public String mostraNinjasd(){
+        return "Todos por Id";
+    }
+    //Update
+    @PutMapping("/alterarNinjaId")
+    public String alterarNinjaId(){
+        return "Alterados";
+    }
+    //Delete
+    @DeleteMapping("/deletaNinjaID")
+    public String deletaNinjaId(){
+        return "Deleta";
     }
 }
