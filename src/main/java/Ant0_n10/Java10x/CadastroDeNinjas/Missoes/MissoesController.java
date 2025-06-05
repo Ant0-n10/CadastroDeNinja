d999725434355349997941f74d570067680cae03
Ant0-n10 <antonio170pina@gmail.com>
2025-06-05 18:17:15 -0300

    feat:Populate the controller with mock data

--- src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,15 @@
 package Ant0_n10.Java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+
+    @GetMapping("/missoes")
+    public String missoes(){
+        return "Controller de Missoes";
+    }
 }
