e1a00106c9cade62ef5398150182b22f64f82116
Ant0-n10 <antonio170pina@gmail.com>
2025-06-11 10:14:59 -0300

    refactor: Convert NinjaRepository class to interface

---
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package Ant0_n10.Java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
