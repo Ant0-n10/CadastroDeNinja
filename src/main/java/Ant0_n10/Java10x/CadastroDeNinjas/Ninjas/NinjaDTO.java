ecb9f99eb1c9773cad67fd0db26a04f86dd2e4f9
Ant0-n10 <antonio170pina@gmail.com>
2025-07-14 22:12:17 -0300

    refactor: Introduce DTO for NinjaModel and map manually in NinjaMapper

--- src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/Ant0_n10/Java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package Ant0_n10.Java10x.CadastroDeNinjas.Ninjas;
 
+import Ant0_n10.Java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private int idade;
+    private String img_url;
+    private String email;
+    private String rank;
+
+    private MissoesModel missoes;
 }
